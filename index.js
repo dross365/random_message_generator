f463d077df465e9abb0b2b3fc89b5da086426aeb
dross365 <dross98.fullstackdev@gmail.com>
2025-04-27 17:53:49 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let messages = ["Come on, you got this!",
+                "Only 5 more minutes!",
+                "You're over half way, keep going!",
+                "You're not going to give up now, are you?",
+                "You were built for this!",
+                "No pain, no gain!",
+                "You showed up, now show me how it's done!",
+                "You're almost there!",
+                "Don't give up on me now, you're so close!",
+                "Mama didn't raise no bi**h, keep going!",
+                "You have to work for the life you want!",
+                "You make your goals!"
+];
+
+let randomMessage = messages[Math.floor(Math.random() * messages.length)];
+console.log(randomMessage);
